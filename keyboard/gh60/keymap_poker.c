6f90b704ff20d58f3d754876bf669d08c344d2f1
Mark Hsu <mark.g.hsu@gmail.com>
2020-03-22 16:52:32 -0700

    mac keyboard version, no more bottom left switch

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -1,16 +1,16 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  KEYMAP(ESC, 1,   2,   3,   4,   5,   PSCR,   F8,   6, 7, 8,   9,   0, APP, \
-	 TAB, Q,   W,   E,   R,   T,   PAUS,  PGUP, Y,  U,   I,   O,  P, BSLS, \
-	 CAPS, A,   S,   D,   F,   G,  GRV, PGDN, H,  J,   K,   L,  SCLN, QUOT,\
-	 LSFT, Z,   X,   C,   V,   B,  FN0,  DEL, MINS, N,  M,  COMM, DOT, SLSH,\
-	 LCTL,LGUI,LALT, LBRC,RBRC,BSPC,RSFT, ENT, SPC, EQL, LEFT, DOWN, UP, RGHT),
+  KEYMAP(ESC, 1,   2,   3,   4,   5,   PAUS,   END,   6, 7, 8,   9,   0, RSFT, \
+	 TAB, Q,   W,   E,   R,   T,   SLCK,  PGUP, Y,  U,   I,   O,  P, BSLS, \
+	 CAPS, A,   S,   D,   F,   G,  QUOT, PGDN, H,  J,   K,   L,  SCLN, GRV,\
+	 LSFT, Z,   X,   C,   V,   B,  DEL,  FN0, MINS, N,  M,  COMM, DOT, SLSH,\
+	 Z  ,LCTL,LALT, LBRC,RBRC,BSPC,LGUI, ENT, SPC, EQL, LEFT, UP, DOWN, RGHT),
   /* 1: Num/Nav */
-  KEYMAP(TRNS,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,F9,F10,F11,  F12, TRNS,\
+  KEYMAP(TRNS,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,F9,F10,F11,  F12, APP,\
 	 TRNS,TRNS,TRNS, UP, TRNS,TRNS,TRNS,TRNS,HOME,7,   8,   9, TRNS,TRNS, \
 	 TRNS,TRNS,LEFT,DOWN,RGHT,CALC,TRNS,TRNS, END,4,   5,   6, TRNS,TRNS,\
-	 TRNS,WHOM,MUTE,VOLU,VOLD,SLCK,TRNS,TRNS,INS, 1,   2,   3, TRNS,TRNS, \
+	 TRNS,WHOM,MUTE,VOLD,VOLU,TRNS,TRNS,TRNS,INS, 1,   2,   3, TRNS,TRNS, \
 	 TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,0, TRNS,TRNS,TRNS,TRNS),
 };
 
